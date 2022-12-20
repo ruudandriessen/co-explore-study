000f9752b0b9324b02feb28b5e574241785018d3
Ruud Andriessen <ruud.andriessen@gmail.com>
2022-12-20 16:54:32 +0100

    feat: parse out group label

--- src/processing/transform.ts
+++ src/processing/transform.ts
@@ -80,11 +80,42 @@ function transformContent(input: string): ContentProperties {
     outputs: outputs ? outputs.split(",").map((output) => output.trim()) : [],
   };
 }
+const GROUP_REGEX = /[Gg]([0-9])[Pp]([0-9,]+)/;
+
+function transformLabels(input: string): {
+  labels: string[];
+  group?: number;
+  participants: number[];
+} {
+  const groupLabel = input.match(GROUP_REGEX);
+
+  if (!groupLabel) {
+    const labels = input.split(",").map((label) => label.trim());
+    return {
+      labels,
+      participants: [],
+    };
+  }
+
+  const [groupLabelContent, group, participants] = groupLabel;
+  const labels = input
+    .replace(groupLabelContent, "")
+    .split(",")
+    .map((label) => label.trim());
+
+  return {
+    labels,
+    group: parseInt(group),
+    participants: participants
+      .split(",")
+      .map((participant) => parseInt(participant)),
+  };
+}
 
 function transformRow(row: InputRow): RowData {
   return {
     activity: row.title,
-    labels: row.labels.split(","),
+    ...transformLabels(row.labels),
     ...transformContent(row.content),
   };
 }
