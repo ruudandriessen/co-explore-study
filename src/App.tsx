6726c0f9a054c34c77f8f8124c42c70509f3577b
Ruud Andriessen <ruud.andriessen@gmail.com>
2023-02-03 17:10:24 +0100

    feat: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useMemo, useState } from "react";
-import { InputRow } from "./models/InputRow";
-import {
-  ChakraProvider,
-  ColorModeScript,
-  extendTheme,
-  Flex,
-} from "@chakra-ui/react";
-import { FileContents } from "./components/FileContents";
-import { Header } from "./components/Header";
-import { transform } from "./processing/transform";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Routes } from "./Routes";
 
 const theme = extendTheme({
   config: {
@@ -17,29 +8,9 @@ const theme = extendTheme({
 });
 
 export function App() {
-  const [inputRows, setInputRows] = useState<InputRow[] | null>(null);
-  const result = useMemo(
-    () => (inputRows ? transform(inputRows) : null),
-    [inputRows]
-  );
-
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={"system"} />
-
-      <Flex padding={2} gap={2} direction="column" maxHeight={"100vh"}>
-        <Header output={result?.output} setInputRows={setInputRows} />
-
-        {inputRows && result ? (
-          <FileContents
-            output={result.output}
-            filtered={result.filtered}
-            rows={inputRows}
-          />
-        ) : (
-          <div>No data</div>
-        )}
-      </Flex>
+      <Routes />
     </ChakraProvider>
   );
 }
