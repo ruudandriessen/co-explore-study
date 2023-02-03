2ca7d44b8c3a75b79dd129c96fe3396a7deac001
Ruud Andriessen <ruud.andriessen@gmail.com>
2023-02-03 19:31:06 +0100

    fix: setup base name

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,3 +1,4 @@
+import { Center, Text } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { DataProcessing } from "./pages/DataProcessing";
 import { Home } from "./pages/Home";
@@ -5,28 +6,39 @@ import { HeaderLayout } from "./pages/layouts/HeaderLayout";
 import { OnlineSurvey } from "./pages/OnlineSurvey";
 import { Papers } from "./pages/Papers";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <HeaderLayout />,
+      errorElement: (
+        <Center height="100vh" flexDir={"column"}>
+          <Text fontSize={"6xl"}>404</Text>
+          <Text>Woops! You seem lost.</Text>
+        </Center>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/data",
+          element: <DataProcessing />,
+        },
+        {
+          path: "/survey",
+          element: <OnlineSurvey />,
+        },
+        {
+          path: "/papers",
+          element: <Papers />,
+        },
+      ],
+    },
+  ],
   {
-    element: <HeaderLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/data",
-        element: <DataProcessing />,
-      },
-      {
-        path: "/survey",
-        element: <OnlineSurvey />,
-      },
-      {
-        path: "/papers",
-        element: <Papers />,
-      },
-    ],
-  },
-]);
+    basename: "/co-explore-study",
+  }
+);
 
 export const Routes = () => <RouterProvider router={router} />;
